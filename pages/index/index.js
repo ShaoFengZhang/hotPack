50d16eb6a77d87c3e6d8719dd0a0504a43ce1725
zhangshaofeng <careerfrontzhang6@163.com>
2018-09-14 15:05:44 +0800

    '添加了随机余额'

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,36 +6,53 @@ Page({
         userInfo: {},
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
-        inputvalue:'',
+        inputvalue: '',
         miniProgram: util.miniProgram,
     },
     onLoad: function() {
         this.canvasbg = '../../assets/canvas.jpg';
-        this.fixedText = '你的尾号2279的账户转账支出人民币12799.00元，活期余额500000.25元[招商银行]';
+        let random = this.RandomNumBoth(1,10);
+        console.log(random);
+        this.fixedText = `你的尾号2279的账户转账支出人民币12799.00元，活期余额${random}00000.25元[招商银行]`;
     },
 
-    inputfocus:function(){
+    RandomNumBoth: function(Min, Max) {
+        var Range = Max - Min;
+        var Rand = Math.random();
+        var num = Min + Math.round(Rand * Range); //四舍五入
+        return num;
+    },
+
+    onShareAppMessage: function(res) {
+        return {
+            title: `快来和我一起装x吧~`,
+            path: `pages/index/index`,
+        };
+    },
+
+
+    inputfocus: function() {
         this.setData({
-            inputvalue: '', 
+            inputvalue: '',
         })
     },
 
-    getinputvalue:function(e){
-        let inputvalue = e.detail.value.slice(0,14);
+    getinputvalue: function(e) {
+        let inputvalue = e.detail.value.slice(0, 14);
         this.setData({
             inputvalue: inputvalue
         })
     },
 
-    startGame:function(){
-        if (this.data.inputvalue==''){
+    startGame: function() {
+        if (this.data.inputvalue == '') {
             wx.showModal({
                 title: '提示',
                 content: '请输入姓名',
                 showCancel: false,
-                success: function (res) { }
+                success: function(res) {}
             })
-        }else{
+        } else {
             wx.showLoading({
                 title: '拼命计算中',
                 mask: true,
@@ -62,44 +79,44 @@ Page({
                 ctx.setFontSize(24);
                 ctx.setFillStyle('#000');
                 ctx.setTextAlign('left');
-                ctx.fillText(_this.contentTxt.slice(0,32), 40, 150);
+                ctx.fillText(_this.contentTxt.slice(0, 32), 40, 150);
                 ctx.fillText(_this.contentTxt.slice(32), 40, 186);
-                
+
                 ctx.draw();
-                setTimeout(function(){
+                setTimeout(function() {
                     _this.showOffRecord();
-                },1000)
+                }, 1000)
 
             },
-            fail:function(res){
+            fail: function(res) {
                 console.log(res)
             }
         })
     },
 
-    showOffRecord: function () {
-        
+    showOffRecord: function() {
+
         wx.canvasToTempFilePath({
             destWidth: this.data.bgimgW * 4,
             destHeight: this.data.bgimgH * 4,
             canvasId: 'canvas',
-            success: function (res) {
+            success: function(res) {
                 wx.hideLoading();
                 wx.saveImageToPhotosAlbum({
                     filePath: res.tempFilePath,
-                    success: function () {
+                    success: function() {
                         wx.showModal({
                             title: '温馨提示',
                             content: '图片已保存成功,快去分享朋友圈吧!',
                             showCancel: false,
-                            success: function (data) {
+                            success: function(data) {
                                 wx.previewImage({
                                     urls: [res.tempFilePath]
                                 })
                             }
                         });
                     },
-                    fail: function () {
+                    fail: function() {
                         wx.hideLoading();
                         wx.previewImage({
                             urls: [res.tempFilePath]
@@ -109,5 +126,6 @@ Page({
             }
         })
     },
+
 
 })
