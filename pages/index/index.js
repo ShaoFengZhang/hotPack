07c57662c2ab8cb7f2ef8b05a9b7c4280718482b
zhangshaofeng <careerfrontzhang6@163.com>
2018-09-14 14:14:04 +0800

    '第一版'

---
+++ pages/index/index.js
@@ -0,0 +1,113 @@
+const app = getApp()
+import util from '../../utils/util.js';
+Page({
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        inputvalue:'',
+        miniProgram: util.miniProgram,
+    },
+    onLoad: function() {
+        this.canvasbg = '../../assets/canvas.jpg';
+        this.fixedText = '你的尾号2279的账户转账支出人民币12799.00元，活期余额500000.25元[招商银行]';
+    },
+
+    inputfocus:function(){
+        this.setData({
+            inputvalue: '', 
+        })
+    },
+
+    getinputvalue:function(e){
+        let inputvalue = e.detail.value.slice(0,14);
+        this.setData({
+            inputvalue: inputvalue
+        })
+    },
+
+    startGame:function(){
+        if (this.data.inputvalue==''){
+            wx.showModal({
+                title: '提示',
+                content: '请输入姓名',
+                showCancel: false,
+                success: function (res) { }
+            })
+        }else{
+            wx.showLoading({
+                title: '拼命计算中',
+                mask: true,
+            });
+            this.contentTxt = this.data.inputvalue + this.fixedText;
+            this.drawcanvs();
+        }
+    },
+
+    drawcanvs: function() {
+        let _this = this;
+        let ctx = wx.createCanvasContext('canvas');
+        console.log(this.canvasbg);
+        wx.getImageInfo({
+            src: this.canvasbg,
+            success: function(res) {
+                _this.setData({
+                    bgimgH: res.height,
+                    bgimgW: res.width,
+                    bgimgUrl: res.path,
+                });
+                ctx.drawImage(_this.canvasbg, 0, 0, res.width, res.height);
+                ctx.drawImage('../../assets/qr.jpg', 50, 1060, 150, 150);
+                ctx.setFontSize(24);
+                ctx.setFillStyle('#000');
+                ctx.setTextAlign('left');
+                ctx.fillText(_this.contentTxt.slice(0,32), 40, 150);
+                ctx.fillText(_this.contentTxt.slice(32), 40, 186);
+                
+                ctx.draw();
+                setTimeout(function(){
+                    _this.showOffRecord();
+                },1000)
+
+            },
+            fail:function(res){
+                console.log(res)
+            }
+        })
+    },
+
+    showOffRecord: function () {
+        
+        wx.canvasToTempFilePath({
+            destWidth: this.data.bgimgW * 4,
+            destHeight: this.data.bgimgH * 4,
+            canvasId: 'canvas',
+            success: function (res) {
+                wx.hideLoading();
+                wx.saveImageToPhotosAlbum({
+                    filePath: res.tempFilePath,
+                    success: function () {
+                        wx.showModal({
+                            title: '温馨提示',
+                            content: '图片已保存成功,快去分享朋友圈吧!',
+                            showCancel: false,
+                            success: function (data) {
+                                wx.previewImage({
+                                    urls: [res.tempFilePath]
+                                })
+                            }
+                        });
+                    },
+                    fail: function () {
+                        wx.hideLoading();
+                        wx.previewImage({
+                            urls: [res.tempFilePath]
+                        })
+                    }
+                })
+            }
+        })
+    },
+
+})
---
+++ utils/util.js
@@ -0,0 +1,26 @@
+
+
+const miniProgram=[
+    {
+        title:'戳戳猜猜',
+        logo:'../../assets/chuo.jpg',
+        appid:'wxc89f64e2e55e28c6',
+        heat:100000,
+    },
+    {
+        title: '萌字泡泡',
+        logo: '../../assets/meng.jpg',
+        appid: 'wx2838f6381433f925',
+        heat: 200000,
+    },
+    {
+        title: '水滴抽奖',
+        logo: '../../assets/shui.jpg',
+        appid: 'wx2ed91bb84c56fe04',
+        heat: 300000,
+    }
+]
+
+module.exports = {
+    miniProgram: miniProgram,
+}
